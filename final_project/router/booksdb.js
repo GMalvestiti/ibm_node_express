6e8348ccaaedb7401455870af8e9d7b0707a37a1
GMalvestiti <gustavo.smalvestiti@gmail.com>
2023-08-18 18:05:31 -0400

    final project completed
    Signed-off-by: GMalvestiti <gustavo.smalvestiti@gmail.com>

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,46 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+    1: {"author": "Chinua Achebe", "title": "Things Fall Apart", "reviews": {
+        "user1": "Review #1 for Things Fall Apart by user1",
+        "user2": "Review #2 for Things Fall Apart by user2",
+        "user3": "Review #3 for Things Fall Apart by user3"
+    }},
+    2: {"author": "Hans Christian Andersen", "title": "Fairy Tales", "reviews": {
+        "user1": "Review #1 for Fairy Tales by user1",
+        "user2": "Review #2 for Fairy Tales by user2"
+    }},
+    3: {"author": "Dante Alighieri", "title": "The Divine Comedy", "reviews": {
+        "user1": "Review #1 for The Divine Comedy by user1",
+        "user2": "Review #2 for The Divine Comedy by user2",
+        "user3": "Review #3 for The Divine Comedy by user3",
+        "user4": "Review #4 for The Divine Comedy by user4"
+    }},
+    4: {"author": "Unknown", "title": "The Epic Of Gilgamesh", "reviews": {
+        "user1": "Review #1 for The Epic Of Gilgamesh by user1",
+        "user2": "Review #2 for The Epic Of Gilgamesh by user2"
+    }},
+    5: {"author": "Unknown", "title": "The Book Of Job", "reviews": {
+        "user1": "Review #1 for The Book Of Job by user1"
+    }},
+    6: {"author": "Unknown", "title": "One Thousand and One Nights", "reviews": {
+        "user1": "Review #1 for One Thousand and One Nights by user1",
+        "user2": "Review #2 for One Thousand and One Nights by user2",
+        "user3": "Review #3 for One Thousand and One Nights by user3"
+    }},
+    7: {"author": "Unknown", "title": "Nj\u00e1l's Saga", "reviews": {
+        "user1": "Review #1 for Nj\u00e1l's Saga by user1"
+    }},
+    8: {"author": "Jane Austen", "title": "Pride and Prejudice", "reviews": {
+        "user1": "Review #1 for Pride and Prejudice by user1",
+        "user2": "Review #2 for Pride and Prejudice by user2"
+    }},
+    9: {"author": "Honor\u00e9 de Balzac", "title": "Le P\u00e8re Goriot", "reviews": {
+        "user1": "Review #1 for Le P\u00e8re Goriot by user1",
+        "user2": "Review #2 for Le P\u00e8re Goriot by user2",
+        "user3": "Review #3 for Le P\u00e8re Goriot by user3"
+    }},
+    10: {"author": "Samuel Beckett", "title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {
+        "user1": "Review #1 for Molloy, Malone Dies, The Unnamable, the trilogy by user1"
+    }}
 }
 
 module.exports=books;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -2,42 +2,118 @@ const express = require('express');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
+const axios = require('axios');
+
 const public_users = express.Router();
 
+const URL = 'https://gustavosmalv-5000.theiadocker-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai';
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.post("/register", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (username && password) {
+        if (!isValid(username)) { 
+            users.push({"username": username, "password": password});
+            return res.status(200).json({message: "User successfully registred. Now you can login."});
+        } else {
+            return res.status(404).json({message: "User already exists!"});    
+        }
+    } 
+
+    return res.status(404).json({message: "Unable to register user."});
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', function (req, res) {
+    res.status(200).json(books);
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+public_users.get('/isbn/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+    res.status(200).json(books[isbn]);
+});
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+    const author = req.params.author;
+    let selectedBooks = {};
+    for (let bookId in books) {
+        if (books[bookId].author == author) {
+            selectedBooks[bookId] = books[bookId];
+        } 
+    }
+    res.status(200).json(selectedBooks);
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/title/:title', function (req, res) {
+    const title = req.params.title;
+    let selectedBooks = {};
+    for (let bookId in books) {
+        if (books[bookId].title == title) {
+            selectedBooks[bookId] = books[bookId];
+        } 
+    }
+    res.status(200).json(selectedBooks);
 });
 
-//  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+// Get book review
+public_users.get('/review/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+    res.status(200).json(books[isbn].reviews);
+});
+
+// Task 10
+public_users.get('/task10', async function (req, res) {
+    try {
+        const response = await axios.get(URL + '/');
+        const books = response.data;
+
+        res.status(200).json(books);
+    } catch (error) {
+        res.status(500).json({error: 'Internal server error'});
+    }
+});
+
+// Task 11
+public_users.get('/task11/:isnb', async function (req, res) {
+    try {
+        const isnb = req.params.isnb;
+        const response = await axios.get(URL + '/isbn/' + isnb);
+        const books = response.data;
+
+        res.status(200).json(books);
+    } catch (error) {
+        res.status(500).json({error: 'Internal server error'});
+    }
+});
+
+// Task 12
+public_users.get('/task12/:author', async function (req, res) {
+    try {
+        const author = req.params.author;
+        const response = await axios.get(URL + '/author/' + author);
+        const books = response.data;
+
+        res.status(200).json(books);
+    } catch (error) {
+        res.status(500).json({error: 'Internal server error'});
+    }
+});
+
+// Task 13
+public_users.get('/task13/:title', async function (req, res) {
+    try {
+        const title = req.params.title;
+        const response = await axios.get(URL + '/title/' + title);
+        const books = response.data;
+
+        res.status(200).json(books);
+    } catch (error) {
+        res.status(500).json({error: 'Internal server error'});
+    }
 });
 
 module.exports.general = public_users;
